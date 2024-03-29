ce922752cb6347dd2da0c725c2e102b51e7fa16e
Emirhan Hasirci <emir_han886@outlook.com>
2024-03-30 00:39:54 +0300

    DbContext and models defined

---
+++ Backend/AnguBlog.API/AnguBlog.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AnguBlog.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnguBlog.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<BlogPost> BlogPosts{ get; set; }
+        public DbSet<Category> Categories{ get; set; }
+    }
+}
