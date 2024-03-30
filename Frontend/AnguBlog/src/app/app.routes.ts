b80cd2d4afa8f53695469188196ac2129b8c98b6
Emirhan Hasirci <emir_han886@outlook.com>
2024-03-30 13:40:55 +0300

    Category list and routing configurations

--- Frontend/AnguBlog/src/app/app.routes.ts
+++ Frontend/AnguBlog/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"Admin/Categories",
+        loadComponent:
+        ()=> import("./features/category/category-list/category-list.component").then(x=>x.CategoryListComponent)
+    }
+];
