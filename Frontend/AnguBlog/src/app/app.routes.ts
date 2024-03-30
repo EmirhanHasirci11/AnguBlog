0a77da890f52828411b942499385f143a3dac71f
Emirhan Hasirci <emir_han886@outlook.com>
2024-03-30 23:53:52 +0300

    Category Add Component and Models defined

--- Frontend/AnguBlog/src/app/app.routes.ts
+++ Frontend/AnguBlog/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+import { AddCategoryComponent } from './features/category/add-category/add-category.component';
 
 export const routes: Routes = [
     {
         path:"Admin/Categories",
         loadComponent:
         ()=> import("./features/category/category-list/category-list.component").then(x=>x.CategoryListComponent)
+    },
+    {
+        path:"Admin/Categories/Add",
+        component:AddCategoryComponent
     }
 ];
---
+++ Frontend/AnguBlog/src/app/features/category/add-category/add-category.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AddCategoryRequest  } from '../models/add-category-request.model';
+
+@Component({
+  selector: 'app-add-category',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './add-category.component.html',
+  styleUrl: './add-category.component.css'
+})
+
+export class AddCategoryComponent {
+
+  model:AddCategoryRequest
+
+  constructor(){
+
+    this.model={
+      name:'Emirhan',
+      urlHandle:''
+    }  
+  }
+  clickBtn(){
+    console.log(this.model.name);
+
+    console.log(this.model.urlHandle);
+  }
+  onFormSubmit() {
+
+  }
+}
