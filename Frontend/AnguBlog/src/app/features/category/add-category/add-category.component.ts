ebc0ce2ad24d717f9d10c58cce16966022cc75a3
Emirhan Hasirci <emir_han886@outlook.com>
2024-04-05 17:09:18 +0300

    Async pipe usage

--- Frontend/AnguBlog/src/app/features/category/add-category/add-category.component.ts
+++ Frontend/AnguBlog/src/app/features/category/add-category/add-category.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { AddCategoryRequest  } from '../models/add-category-request.model';
 import { CategoryService } from '../services/category.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-category',
@@ -16,7 +17,7 @@ export class AddCategoryComponent implements OnDestroy {
 
   model:AddCategoryRequest
   private addCategorySubscription? : Subscription
-  constructor(private categoryService:CategoryService){
+  constructor(private categoryService:CategoryService,private router:Router){
 
     this.model={
       name:'',
@@ -29,8 +30,7 @@ export class AddCategoryComponent implements OnDestroy {
   onFormSubmit() {
     this.addCategorySubscription= this.categoryService.addCategory(this.model).subscribe({
       next:(response)=>{
-        console.log(response);
-        console.log("Category Creation is succeed");        
+        this.router.navigateByUrl('/Admin/Categories')
       }
     });
   }
--- Frontend/AnguBlog/src/app/features/category/category-list/category-list.component.ts
+++ Frontend/AnguBlog/src/app/features/category/category-list/category-list.component.ts
@@ -3,6 +3,7 @@ import { RouterLink } from '@angular/router';
 import { CategoryService } from '../services/category.service';
 import { Category } from '../models/category.model';
 import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-category-list',
@@ -12,14 +13,14 @@ import { CommonModule } from '@angular/common';
   styleUrl: './category-list.component.css'
 })
 export class CategoryListComponent implements OnInit {
-  Categories?:Category[];
+  categories$?:Observable<Category[]>;
+
   constructor(private categoryService:CategoryService){
 
   }
   ngOnInit(): void {
-    this.categoryService.getAllCategories().subscribe({next:(response)=>{
-      this.Categories=response;
-    }})
+    this.categories$= this.categoryService.getAllCategories();
+    
   }
 
 }
